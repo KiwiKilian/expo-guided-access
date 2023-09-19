690b05423d4e4ea136f8d5a92dfd38f4f127c57e
Kilian Finger <hey@kilianfinger.com>
2023-09-19 22:49:40 +0200

    style: fix linting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
 
-import ExpoGuidedAccessModule from './ExpoGuidedAccessModule';
 import { GuidedAccessEnabledChangeEventPayload } from './ExpoGuidedAccess.types';
+import ExpoGuidedAccessModule from './ExpoGuidedAccessModule';
 
 export async function isGuidedAccessEnabled(): Promise<boolean> {
   return await ExpoGuidedAccessModule.isGuidedAccessEnabled();
